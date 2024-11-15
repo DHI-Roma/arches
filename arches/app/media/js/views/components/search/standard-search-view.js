0bdb2d4c0b7d375875a32e3a3abb83b6380516d2
Galen <whatisgalen@gmail.com>
2024-11-15 11:32:51 -0800

    define searchQueryId, override doQuery in standard-search-view, re #10502

--- arches/app/media/js/views/components/search/standard-search-view.js
+++ arches/app/media/js/views/components/search/standard-search-view.js
@@ -15,6 +15,8 @@ define([
             
             this.selectedPopup = ko.observable('');
             this.sharedStateObject.selectedPopup = this.selectedPopup;
+            this.searchQueryId = ko.observable(null);
+            this.sharedStateObject.searchQueryId = this.searchQueryId;
             var firstEnabledFilter = _.find(this.sharedStateObject.searchFilterConfigs, function(filter) {
                 return filter.config.layoutType === 'tabbed';
             }, this);
@@ -51,6 +53,47 @@ define([
             this.searchFilterVms[componentName](this);
         },
 
+        doQuery: function() {
+            const queryObj = JSON.parse(this.queryString());
+            if (self.updateRequest) { self.updateRequest.abort(); }
+            self.updateRequest = $.ajax({
+                type: "GET",
+                url: arches.urls.search_results,
+                data: queryObj,
+                context: this,
+                success: function(response) {
+                    _.each(this.sharedStateObject.searchResults, function(value, key, results) {
+                        if (key !== 'timestamp') {
+                            delete this.sharedStateObject.searchResults[key];
+                        }
+                    }, this);
+                    _.each(response, function(value, key, response) {
+                        if (key !== 'timestamp') {
+                            this.sharedStateObject.searchResults[key] = value;
+                        }
+                    }, this);
+                    this.sharedStateObject.searchResults.timestamp(response.timestamp);
+                    this.searchQueryId(this.sharedStateObject.searchResults.searchqueryid);
+                    this.sharedStateObject.userIsReviewer(response.reviewer);
+                    this.sharedStateObject.userid(response.userid);
+                    this.sharedStateObject.total(response.total_results);
+                    this.sharedStateObject.hits(response.results.hits.hits.length);
+                    this.sharedStateObject.alert(false);
+                },
+                error: function(response, status, error) {
+                    const alert = new AlertViewModel('ep-alert-red', arches.translations.requestFailed.title, response.responseJSON?.message);
+                    if(self.updateRequest.statusText !== 'abort'){
+                        this.alert(alert);
+                    }
+                    this.sharedStateObject.loading(false);
+                },
+                complete: function(request, status) {
+                    self.updateRequest = undefined;
+                    window.history.pushState({}, '', '?' + $.param(queryObj).split('+').join('%20'));
+                    this.sharedStateObject.loading(false);
+                }
+            });
+        },
     });
 
     return ko.components.register(componentName, {
