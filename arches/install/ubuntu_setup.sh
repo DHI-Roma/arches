6ffa764a146c713e2b6dc260f6440d6aee853e91
Galen <whatisgalen@gmail.com>
2025-02-21 19:58:38 -0800

    lock es version to 8.17.0 for all deps

--- arches/install/ubuntu_setup.sh
+++ arches/install/ubuntu_setup.sh
@@ -57,7 +57,7 @@ function install_elasticsearch {
   wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
   sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" > /etc/apt/sources.list.d/elastic-8.x.list'
   sudo apt-get update
-  sudo apt-get install elasticsearch
+  sudo apt-get install elasticsearch=8.17.0
   sudo /bin/systemctl enable elasticsearch.service
   sudo systemctl enable elasticsearch.service
   sudo systemctl start elasticsearch.service
