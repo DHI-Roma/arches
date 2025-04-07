a2fd0a30edc4fb435106e42d05fa4be6df6bece8
Galen <whatisgalen@gmail.com>
2025-04-07 16:58:59 -0700

    include resourceName property on default_es_mapping for resource-instance dt, re #11939

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -2343,6 +2343,10 @@ class ResourceInstanceDataType(BaseDataType):
                     "type": "text",
                     "fields": {"keyword": {"ignore_above": 256, "type": "keyword"}},
                 },
+                "resourceName": {
+                    "type": "text",
+                    "fields": {"keyword": {"ignore_above": 256, "type": "keyword"}},
+                },
                 "ontologyProperty": {
                     "type": "text",
                     "fields": {"keyword": {"ignore_above": 256, "type": "keyword"}},
