617e35c4350ae974e866b85ecc1f05785d06f6c7
Galen <whatisgalen@gmail.com>
2025-05-09 17:01:05 -0700

    otpimize_resource_iteration, indexing in transaction editlog-based deletion, update, re #12015

--- arches/app/utils/transaction.py
+++ arches/app/utils/transaction.py
@@ -1,7 +1,12 @@
 import logging
+import uuid
 from arches.app.models.resource import Resource
 from arches.app.models.tile import Tile
 from arches.app.models.models import IIIFManifest, EditLog, WorkflowHistory
+from arches.app.utils.index_database import (
+    index_resources_by_transaction,
+    optimize_resource_iteration,
+)
 from django.db import transaction, DatabaseError
 
 # Get an instance of a logger
@@ -10,32 +15,64 @@ logger = logging.getLogger(__name__)
 
 # Given a transaction ID, reverse (delete or update) tiles and resources created/updated during the transaction
 def reverse_edit_log_entries(transaction_id):
-    transaction_changes = (
-        EditLog.objects.filter(transactionid=transaction_id)
-        .order_by("-timestamp")
-        .all()
+    revserse_operation_transactionid = str(uuid.uuid4())
+
+    transaction_changes = EditLog.objects.filter(transactionid=transaction_id)
+
+    resource_create_changes = transaction_changes.filter(edittype="create")
+    tile_edit_changes = transaction_changes.filter(edittype="tile edit")
+    tile_create_changes = transaction_changes.filter(edittype="tile create")
+
+    number_of_db_changes = (
+        resource_create_changes.count()
+        + tile_edit_changes.count()
+        + tile_create_changes.count()
     )
-    number_of_db_changes = 0
+
+    created_resources_query_set = Resource.objects.filter(
+        resourceinstanceid__in=resource_create_changes.values_list(
+            "resourceinstanceid", flat=True
+        )
+    )
+
     try:
         with transaction.atomic():
-            for edit_log in transaction_changes:
-                if edit_log.edittype == "create":
-                    for obj in Resource.objects.filter(
-                        resourceinstanceid=edit_log.resourceinstanceid
-                    ):
-                        obj.delete()
-                        number_of_db_changes += 1
-                elif edit_log.edittype == "tile create":
-                    for obj in Tile.objects.filter(tileid=edit_log.tileinstanceid):
-                        obj.delete()
-                        number_of_db_changes += 1
-                elif edit_log.edittype == "tile edit":
-                    for obj in Tile.objects.filter(tileid=edit_log.tileinstanceid):
-                        obj.data = edit_log.oldvalue
-                        obj.save()
-                        number_of_db_changes += 1
+            for resource in optimize_resource_iteration(
+                created_resources_query_set, chunk_size=2000
+            ):
+                resource.delete(
+                    fetch_relations=False,
+                    transaction_id=revserse_operation_transactionid,
+                )
+
+            for tile in Tile.objects.filter(
+                tileid__in=tile_create_changes.values_list("tileinstanceid", flat=True)
+            ):
+                tile.delete(
+                    recalculate_descriptors=False,
+                    transaction_id=revserse_operation_transactionid,
+                )
+
+            for tile in Tile.objects.filter(
+                tileid__in=tile_edit_changes.values_list("tileinstanceid", flat=True)
+            ):
+                tile.data = tile_edit_changes.get(
+                    tileinstanceid=str(tile.tileid)
+                ).oldvalue
+                tile.save(
+                    index=False,
+                    recalculate_descriptors=False,
+                    transaction_id=revserse_operation_transactionid,
+                )
+        if tile_edit_changes.count() > 0:
+            index_resources_by_transaction(
+                transaction_id,
+                recalculate_descriptors=True,
+            )
+
     except DatabaseError:
         logger.error("Error connecting to database")
+        number_of_db_changes = 0
 
     return number_of_db_changes
 
